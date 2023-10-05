019c52dadd0fdc9a6d1171bb63302608a9042551
mouad-elamoum <elamoum.mouad@gmail.com>
2023-10-05 11:51:37 +0200

    1ere correction de l'exo

--- exercice5.sh
+++ exercice5.sh
@@ -1,7 +1,4 @@
-x = int (input("premier chiffre"))
-operateur = (input("l'operateur"))
-y = int (input("deuxieme chiffre"))
-
-def calcule (x,operateur,y):
-    if operateur == '+' :
-        print (x+y)
+read ' entrez le premier nombre' var1
+read 'entrez le deuxieme nombre' var2
+som= var1+var2
+echo $ som
